8564c86a9ef24161a00bf126da42ac39e28ac7bc
KaichiIwahara <157882052+kai159874@users.noreply.github.com>
2024-03-02 04:27:19 +0000

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  
+  get 'users/show'
+  get 'users/edit'
+  devise_for :users
+  
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorite, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
+  end
+  resources :users, only: [:show, :edit, :update]
+  
+
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: "homes#top"
+  get "homes/about" => "homes#about", as: "about"
+end
